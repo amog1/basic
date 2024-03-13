ef449c2b31402eafd9dbbcaf3ca9ec70c02587ba
amog1 <160067063+amog1@users.noreply.github.com>
2024-03-13 13:52:51 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+// INTRO TO JAVASCRIPT
+
+// Input Process Output (IPO)
+
+// Output: Alert & Console
+
+console.log("flying");
+
+// Input: Prompt
+
+// Processing: Data Types
+
+// Processing: Variables
+
+// var userHeight;
+// let userFavourColour;
+
+// let user_height = 175;
+// var user_name = "jesse";
+
+// const capital = "vancover";
+
+// userHeight = "6";
+// user_name = "Cloud";
+
+// console.log(userHeight);
+// console.log(user_name);
+
+// String Concatenation & Template Strings
+// let output = user_name + "s height is " + userHeight + " feet.";
+// Template Strings
+// console.log(output);
+// let output2 = "${user_name} lives in ${capital} .";
+
+// let output = user_name + "s height is " + userHeight + " feet.";
+
+// console.log(output);
+// let output2 = "${user_name} lives in ${capital} .";
+
+// Comments
+
+// Debugging and Testing
+
+// Mad Lib
+
+// Input
+// var adjecttive;
+// var noun;
+// var number;
+// var verb;
+
+// noun = prompt("Enter a noun:");
+// adjective = prompt("Enter an adjective:");
+// number = prompt("Enter an adejctive:");
+// verb = prompt("Enter a verb:");
+
+// Processing
+// let output = `The ${adjective} ${noun} can ${verb}  ${number}
+// ${number} kilometer    `;
+
+// Output
+
+// var pizza_size;
+// var topping_one;
+// var topping_two;
+
+// pizza_size = prompt("Enter a pizza size");
+// topping_one = prompt("Enter your first topping");
+// topping_two = prompt("Enter the second topping");
+
+var course_1;
+var mark_1;
+var course_2;
+var mark_2;
+
+course_1 = prompt("Enter your course 1");
+mark_1 = prompt("Enter your grade 1");
+course_2 = prompt("Enter your course 2");
+mark_2 = prompt("Enter your grade 2");
+
+// var adjecttive_1;
+// var adjecttive_2;
+// var noun;
+// var verb;
+// var plural_noun;
+
+// noun = prompt("Enter a noun:");
+// adjecttive_1 = prompt("Enter an adjective_1:");
+// adjecttive_2 = prompt("Enter an adjective_2:");
+// verb = prompt("Enter a verb:");
+// plural_noun = prompt("Enter a plural_noun:");
